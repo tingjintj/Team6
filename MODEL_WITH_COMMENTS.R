48d55639c4ade398d99f757da3cb1499c4c95f07
tingjintj <159521627+tingjintj@users.noreply.github.com>
2024-04-13 14:29:40 +0200

    Add files via upload

---
+++ MODEL_WITH_COMMENTS.R
@@ -0,0 +1,128 @@
+#load libraries
+library(tidyverse)
+library(tidymodels)
+library(data.table)
+library(corrplot)
+library(kknn)
+library(vip)        
+library(probably)   
+library(scales)
+library(janitor)    
+library(themis)     
+library(parallel)
+library(doParallel)
+
+num_cores <- parallel::detectCores(logical = FALSE)
+cl <- makeCluster(num_cores - 1) 
+registerDoParallel(cl)
+
+# Load the data --------------------------------------------------------------
+# The data has been preprocessed in a separate file for organization and maintainability.
+# We're now loading the cleaned data set, which is stored as an RDS file on GitHub, to train the model.
+# This approach separates data preprocessing from model training, improving code readability and modularity.
+
+url <- "https://raw.githubusercontent.com/tingjintj/Team6/main/data_final.RData"
+temp_file <- tempfile()
+download.file(url, temp_file, mode = "wb")
+load(temp_file) # For .RData
+# df <- readRDS(temp_file) # For .RDS
+unlink(temp_file)
+
+# MODEL -----------------------------------------------------------------------
+
+## Training and Testing sets -------------------------------------------------
+
+set.seed(123)
+data_split <- initial_split(df, prop = 0.80)
+train_data <- training(data_split)
+test_data  <- testing(data_split)
+
+# Adjust factor levels for both 'make' and 'model' in training and testing data
+all_make_levels <- levels(df$make)
+all_model_levels <- levels(df$model)
+
+train_data <- train_data %>%
+  mutate(
+    make = factor(make, levels = all_make_levels),
+    model = factor(model, levels = all_model_levels)
+  )
+
+test_data <- test_data %>%
+  mutate(
+    make = factor(make, levels = all_make_levels),
+    model = factor(model, levels = all_model_levels)
+  )
+
+str(train_data)
+str(test_data)
+
+## RANDOM FOREST MODEL --------------------------------------------------------
+
+### Model specification ------------------------------------------------------
+
+rf_spec <- rand_forest(trees = 500, mtry = tune(), min_n = tune()) %>%
+  set_mode("regression") %>%
+  set_engine("ranger", importance = 'permutation')
+
+### Create a recipe for preprocessing ----------------------------------------
+recipe <- recipe(price ~ ., data = train_data)
+
+### Set up 5-fold cross-validation -------------------------------------------
+cv_folds <- vfold_cv(train_data, v = 5)
+
+#### Define a tuning ---------------------------------------------------------
+grid <- grid_latin_hypercube(
+  mtry(range = c(2, round(sqrt(ncol(train_data)), 0))),
+  min_n(range = c(5, 20)),
+  size = 30
+)
+
+### Tune the model -----------------------------------------------------------
+tuned_results <- tune_grid(
+  workflow() %>%
+    add_recipe(recipe) %>%
+    add_model(rf_spec),
+  resamples = cv_folds,
+  grid = grid,
+  control = control_grid(save_pred = TRUE, save_workflow = TRUE)
+)
+
+# First, collect all metrics from the tuning results
+all_metrics <- collect_metrics(tuned_results)
+
+# Filter for the lowest RMSE
+best_rmse <- all_metrics %>%
+  filter(.metric == "rmse") %>%
+  arrange(mean) %>%
+  slice(1)
+
+print(best_rmse) # From this object it is possible to visualize the best parameters
+
+# FINAL MODEL -----------------------------------------------------
+
+rf_spec <- rand_forest(
+  trees = 500,    # This is fixed
+  mtry = 3,       # Best parameter from tuning
+  min_n = 6       # Best parameter from tuning
+) %>% 
+  set_mode("regression") %>%
+  set_engine("ranger")
+
+## Fitting the model on training data ----------------------------------------
+final_model <- rf_spec %>% 
+  fit(price ~ ., data = train_data)
+
+saveRDS(final_model, "/Users/tingjin/Downloads/final_model.rds")
+
+## Evaluate on test data ------------------------------------------------------
+test_results <- predict(final_model, test_data) %>%
+  bind_cols(test_data) %>%
+  metrics(truth = price, estimate = .pred)
+
+# Print the MAE for the test set
+test_results %>% filter(.metric == "mae")
+
+# Stop and clear parallel processing
+stopCluster(cl)
+registerDoSEQ()
+
---
+++ shiny_app_cars_data_preprocessing.R
@@ -0,0 +1,49 @@
+# Load libraries
+library(tidyverse)
+library(data.table)
+library(janitor)
+
+# Import Data
+df <- read.csv("/Users/joelkoemen/Desktop/R Final Project/autoscout24-germany-dataset.csv")
+
+# Drop missing values
+df <- df %>% drop_na()
+
+# Check on unique values for each categorical variable, to ensure that there are no doublings etc.
+unique(df$make)
+unique(df$model)
+unique(df$fuel)
+unique(df$gear)
+unique(df$offerType)
+unique(df$year)
+
+# Show samples where df$make is "Caravans-Wohnm"
+df %>% filter(make == "Caravans-Wohnm") %>% head()
+
+# Based on research, we know that "Caravans-Wohnm" is a model of "VW". Therefore, 
+# we first add "Caravans-Wohnm" as a model in those instances where the make is "Caravans-Wohnm"
+# and then add VW as the actual make of those models
+df$model[df$make == "Caravans-Wohnm"] <- "Caravans-Wohnm"
+df$make[df$make == "Caravans-Wohnm"] <- "VW"
+
+# Show samples where df$make is "Trailer-Anhänger"
+df %>% filter(make == "Trailer-Anhänger") %>% head()
+
+# We decide to drop instances with make "Trailer-Anhänger" as they are not 
+# relevant for our analysis (they are not cars)
+df <- df %>% filter(make != "Trailer-Anhänger")
+
+# Convert categorical values into factors
+data_final <- df %>%
+  mutate(
+    make = factor(make, levels = unique(df$make)),
+    model = factor(model, levels = unique(df$model)),
+    fuel = factor(fuel, levels = unique(df$fuel)),
+    gear = factor(gear, levels = unique(df$gear)),
+    offerType = factor(offerType, levels = unique(df$offerType))
+  )
+
+save(data_final, file = '/Users/joelkoemen/Desktop/R Final Project/data_final.RData')
+
+
+
